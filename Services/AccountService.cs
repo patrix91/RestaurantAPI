27d801b9a0ca8c550e5aa8126098bec45eba0708
Patryk Wiśniewski <patrix91@users.noreply.github.com>
2023-08-06 15:19:20 +0200

    dodanie mechanizmu hashowania hasła użytkownika

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantAPI.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using RestaurantAPI.Entities;
 using RestaurantAPI.Model;
 
 namespace RestaurantAPI.Services
@@ -10,10 +11,12 @@ namespace RestaurantAPI.Services
     public class AccountService : IAccountService
     {
         private readonly RestaurantDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
 
-        public AccountService(RestaurantDbContext context)
+        public AccountService(RestaurantDbContext context, IPasswordHasher<User> passwordHasher)
         {
             _context = context;
+            _passwordHasher = passwordHasher;
         }
         public void RegisterUser(RegisterUserDto dto)
         {
@@ -24,6 +27,10 @@ namespace RestaurantAPI.Services
                 DateOfBirth = dto.DateOfBirth,
                 RoleId = dto.RoleId
             };
+
+            var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
+
+            newUser.PasswordHash = hashedPassword;
             _context.Users.Add(newUser);
             _context.SaveChanges();
         }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,7 @@ namespace RestaurantAPI
             services.AddScoped<IRestaurantService, RestaurantServices>();
             services.AddScoped<IDishService, DishService>();
             services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<ErrorHandlingMiddleware>();
             services.AddScoped<RequestTimeMiddleware>();
         }
